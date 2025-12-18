623c8c17b52bc931c35452082ad779a53deefd63
binbinmods <binbinmods@gmail.com>
2025-12-17 22:54:32 -0500

    v0.1.0 Initial attempt

---
+++ SpeedUpFunctions.cs
@@ -0,0 +1,48 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+// using Obeliskial_Content;
+// using Obeliskial_Essentials;
+using System.IO;
+using static UnityEngine.Mathf;
+using UnityEngine.TextCore.LowLevel;
+using static SpeedUp.Plugin;
+using System.Collections.ObjectModel;
+using UnityEngine;
+
+namespace SpeedUp
+{
+    public class SpeedUpFunctions
+    {
+
+        public static void HandleSpeedChange()
+        {
+            Time.timeScale = SpeedModifier.Value;
+        }
+
+        public static void HandleUltraFast()
+        {
+            if (EnableUltrafast.Value)
+            {
+                GameManager.Instance.configGameSpeed = Enums.ConfigSpeed.Ultrafast;
+                return;
+            }
+
+            bool flag = SaveManager.PrefsHasKey("fastMode") ? SaveManager.LoadPrefsBool("fastMode") : (SettingsManager.Instance?.fastModeToggle.isOn ?? false);
+            SettingsManager.Instance?.fastModeToggle?.isOn = flag;
+            if (flag)
+            {
+                GameManager.Instance.configGameSpeed = Enums.ConfigSpeed.Fast;
+            }
+            else
+            {
+                GameManager.Instance.configGameSpeed = Enums.ConfigSpeed.Slow;
+            }
+
+        }
+
+
+    }
+}
+
---
+++ SpeedUpPatches.cs
@@ -0,0 +1,38 @@
+using BepInEx;
+using BepInEx.Logging;
+using BepInEx.Configuration;
+using HarmonyLib;
+// using static Obeliskial_Essentials.Essentials;
+using System;
+using static SpeedUp.Plugin;
+using static SpeedUp.CustomFunctions;
+using static SpeedUp.SpeedUpFunctions;
+using System.Collections.Generic;
+using static Functions;
+using UnityEngine;
+// using Photon.Pun;
+using TMPro;
+using System.Linq;
+using System.Xml.Serialization;
+using System.Text.RegularExpressions;
+using System.Reflection;
+using System.Diagnostics;
+// using Unity.TextMeshPro;
+
+// Make sure your namespace is the same everywhere
+namespace SpeedUp
+{
+
+    [HarmonyPatch] // DO NOT REMOVE/CHANGE - This tells your plugin that this is part of the mod
+
+    public class SpeedUpPatches
+    {
+        public static bool devMode = false; //DevMode.Value;
+        public static bool bSelectingPerk = false;
+        public static bool IsHost()
+        {
+            return GameManager.Instance.IsMultiplayer() && NetworkManager.Instance.IsMaster();
+        }
+
+    }
+}
